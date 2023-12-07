b1e39490b355f9d88cad1a3700bf9edd8eb2ba38
xosblo91 <xosblo91@gmail.com>
2023-12-07 14:02:40 +0100

    day07 - prep

--- cmd/day07/main.go
+++ cmd/day07/main.go
@@ -9,66 +9,105 @@ import (
 )
 
 var cardWeights = map[string]int{
-	"2": 2,
-	"3": 3,
-	"4": 4,
-	"5": 5,
-	"6": 6,
-	"7": 7,
-	"8": 8,
-	"9": 9,
-	"T": 10,
-	"J": 11,
-	"Q": 12,
-	"K": 13,
-	"A": 14,
+	"2": 1,
+	"3": 2,
+	"4": 3,
+	"5": 4,
+	"6": 5,
+	"7": 6,
+	"8": 7,
+	"9": 8,
+	"T": 9,
+	"J": 10,
+	"Q": 11,
+	"K": 12,
+	"A": 13,
 }
 
-const (
-	FI = "FiveOfAKind"
-	FO = "FourOfAKind"
-	FH = "FullHouse"
-	TH = "ThreeOfAKind"
-	TP = "TwoPair"
-	OP = "OnePair"
-	HC = "HighCard"
-)
-
-var handStrength = map[string]int{
-	HC: 1,
-	OP: 2,
-	TP: 3,
-	TH: 4,
-	FH: 5,
-	FO: 6,
-	FI: 7,
-}
+// const (
+// 	HC = "HighCard"
+// 	OP = "OnePair"
+// 	TP = "TwoPair"
+// 	TH = "ThreeOfAKind"
+// 	FH = "FullHouse"
+// 	FO = "FourOfAKind"
+// 	FI = "FiveOfAKind"
+// )
+//
+// var handStrength = map[string]int{
+// 	HC: 1, // 1 2 3 4 5 - 5
+// 	OP: 2, // 1 1 3 4 5 - 4
+// 	TP: 3, // 1 1 2 2 5 - 3
+// 	TH: 4, // 1 1 1 4 5 - 3
+// 	FH: 5, // 1 1 1 2 2 - 2
+// 	FO: 6, // 1 1 1 1 2 - 2
+// 	FI: 7, // 1 1 1 1 1 - 1
+// }
 
 type game struct {
-	cards   string
-	betSize int
+	cards    string
+	betSize  int
+	strength int
 }
 
-func (g *game) identifyHand() {
+func (g *game) setHandStrength() {
+	count := make(map[string]int)
+	for _, card := range g.cards {
+		if _, exist := count[string(card)]; !exist {
+			count[string(card)] = 1
+		} else {
+			t := count[string(card)]
+			t++
+			count[string(card)] = t
+		}
+	}
 
+	switch len(count) {
+	case 1:
+		g.strength = 7
+	case 2:
+		for _, c := range count {
+			if c == 4 {
+				g.strength = 6
+				return
+			}
+		}
+		g.strength = 5
+	case 3:
+		for _, c := range count {
+			if c == 3 {
+				g.strength = 4
+				return
+			}
+		}
+		g.strength = 3
+	case 4:
+		g.strength = 2
+	case 5:
+		g.strength = 1
+	}
 }
 
-func part1(games []game) int {
+func part1(games []*game) int {
+	for _, g := range games {
+		g.setHandStrength()
+	}
+
 	return 0
 }
 
-func readInput() []game {
+func readInput() []*game {
 	f, err := os.Open("input.txt")
 	if err != nil {
 		panic("fuck")
 	}
 
-	games := make([]game, 0)
+	games := make([]*game, 0)
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		split := strings.Split(scanner.Text(), " ")
-		games = append(games, game{
+		games = append(games, &game{
 			cards:   split[0],
 			betSize: conv.Atoi[int](split[1]),
 		})
